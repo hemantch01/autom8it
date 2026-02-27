e55e499e7fba660f29deac25b5c6bfe0f519d830
hemantch01 <hemantchaudhary905@gmail.com>
2026-02-27 18:32:00 +0000

    added react-flow basic code

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -2,6 +2,21 @@
 
 import { ErrorView, LoadingView } from "@/components/byMe/entity-component";
 import { useSuspenseWorkflow } from "@/features/workflows/hooks/use-workflows";
+import {
+    ReactFlow,
+    applyNodeChanges,
+    applyEdgeChanges,
+    addEdge,
+    type Node,
+    type Edge,
+    NodeChange,
+    EdgeChange,
+    Connection,
+    Background
+} from "@xyflow/react";
+import "@xyflow/react/dist/style.css"
+import { useCallback, useState } from "react";
+
 export const EditorLoading = ()=>{
     return <LoadingView message="Loading editor..."/>
 }
@@ -9,13 +24,64 @@ export const EditorError = ()=>{
     return <ErrorView message="Error Loading editor"/>
 }
 
+const initialNodes = [
+    {
+        id:"n1",
+        position:{x:0,y:0},
+        data: {
+            label:"Node 1"
+        }
+    },
+    {
+      id:'n2',
+      position:{ x:0,y:100},
+      data:{
+        label:"Node 2"
+      }  
+    }
+];
+
+const initialEdges = [
+    {
+        id:"n1-n2",
+        source:"n1",
+        target:"n2",
+    }
+];
+
 export const Editor = ({workflowId}:{workflowId:string})=>{
     const { data: workflow} = useSuspenseWorkflow(workflowId);
     
-    return (
-        <p>
-            {JSON.stringify(workflow,null,2)}
+    const [nodes, setNodes]  = useState<Node[]>(initialNodes);
+    const [edges, setEdges] = useState<Edge[]>(initialEdges);
+   const onNodesChange = useCallback(
+        (changes:NodeChange[])=> setNodes(
+            (nodeSnapShot)=> applyNodeChanges(changes,nodeSnapShot)
+        )
+        ,[]);
+    const onEdgesChange = useCallback(
+        (changes:EdgeChange[])=> setEdges(
+            (edgesSnapShot)=> applyEdgeChanges(changes,edgesSnapShot)
+        )
+        ,[]);
 
-        </p>
+    const onConnect = useCallback(
+        (params:Connection)=> setEdges(
+            (edgesSnapShot)=> addEdge(params,edgesSnapShot)
+        )
+        ,[]);
+    return (
+        <div className="size-full">
+            <ReactFlow
+            nodes={nodes}
+            edges={edges}
+            onNodesChange={onNodesChange}
+            onEdgesChange={onEdgesChange}
+            onConnect={onConnect}
+            fitView
+            >
+                <Background/>
+            </ReactFlow>
+        </div>
     )
 }
