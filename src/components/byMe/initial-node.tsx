bf488e99e8ed0b7d089971654c4468998e8a4f8c
hemantch01 <hemantchaudhary905@gmail.com>
2026-02-28 18:39:16 +0000

    added sheet to select trigger from node and button

--- src/components/byMe/initial-node.tsx
+++ src/components/byMe/initial-node.tsx
@@ -5,18 +5,23 @@ import { PlusIcon } from "lucide-react";
 import {memo, useState} from "react";
 import { PlaceholderNode } from "../react-flow/placeholder-node";
 import { WorkflowNode } from "../react-flow/workflow-node";
+import { NodeSelector } from "../react-flow/node-selector";
 
 export const InitialNode = memo((props: NodeProps)=>{
+    const [selectorOpen, setSelectorOpen] = useState(false);
     return (
-       <WorkflowNode showToolbar={true} name="initial node" description="thiws is ">
+       <NodeSelector open={selectorOpen} onOpenChange={setSelectorOpen}>
+        <WorkflowNode showToolbar={true} name="initial node" description="thiws is ">
          <PlaceholderNode
         {...props}
+        onClick={()=> setSelectorOpen(true)}
         >
             <div className="cursor-pointer flex items-center justify-center">
                 <PlusIcon className="size-4"/>
             </div>
         </PlaceholderNode>
        </WorkflowNode>
+       </NodeSelector>
     )
 });
 
--- src/components/react-flow/add-node-button.tsx
+++ src/components/react-flow/add-node-button.tsx
@@ -1,18 +1,22 @@
 "use client";
 
-import {memo} from "react";
+import {memo, useState} from "react";
 import {Button } from "@/components/ui/button";
 import { PlusIcon } from "lucide-react";
+import { NodeSelector } from "./node-selector";
 
 export const AddNodeButton = memo(()=>{
+    const [selectorOpen,setSelectorOpen] = useState(false);
     return (
-        <Button
+        <NodeSelector open = {selectorOpen} onOpenChange={setSelectorOpen}>
+            <Button
         onClick={()=> {}}
         size={"icon"}
         variant={"outline"}
         className="bg-background">
         <PlusIcon/>
         </Button>
+        </NodeSelector>
     )
 });
 
