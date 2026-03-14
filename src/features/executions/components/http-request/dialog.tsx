b4bd4e2e67cb0e13433d9bc7e4ee42f2cf0ffb46
hemantch01 <hemantchaudhary905@gmail.com>
2026-03-14 20:08:59 +0000

    fixed the overwriting of previous node data
    Signed-off-by: hemantch01 <hemantchaudhary905@gmail.com>

--- src/features/executions/components/http-request/dialog.tsx
+++ src/features/executions/components/http-request/dialog.tsx
@@ -33,6 +33,12 @@ import { useEffect } from "react";
 import { Button } from "@/components/ui/button";
 
 const formSchema = z.object({
+  variableName:z
+  .string()
+  .min(1,{message:"variable name is required"})
+  .regex(/^[A-Za-z_$][A-Za-z0-9_$]*$/,{
+    message:"variable name must start with a letter or underscore and container only letters, numbers, underscores"
+  }),
   endpoint: z.url({ message: "Please enter a valid URL" }),
   method: z.enum(["GET", "POST", "PUT", "PATCH", "DELETE"]),
   body: z
@@ -59,6 +65,7 @@ export const HttpRequestDialog = ({
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
+      variableName:defaultValues?.variableName||"",
       endpoint: defaultValues?.endpoint || "",
       method: defaultValues?.method || "GET",
       body: defaultValues?.body ||"",
@@ -69,13 +76,14 @@ export const HttpRequestDialog = ({
   useEffect(() => {
     if (open) {
       form.reset({
+        variableName: defaultValues?.variableName||"",
         endpoint: defaultValues?.endpoint || "",
         method: defaultValues?.method || "GET",
         body: defaultValues?.body ||"",
       });
     }
   }, [open, defaultValues, form]);
-
+  const watchVariableName= form.watch("variableName")||"myApiCall";
   const watchMethod = form.watch("method");
   const showBodyField = ["POST", "PUT", "PATCH"].includes(watchMethod);
 
@@ -98,6 +106,25 @@ export const HttpRequestDialog = ({
             onSubmit={form.handleSubmit(handleSubmit)}
             className="space-y-8 mt-4"
           >
+            <FormField
+              control={form.control}
+              name="variableName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Variable Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="myApiCall"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    Use this name to reference the result in other nodes:{" "}{`{{${watchVariableName}.httpResponse.data}}`}
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
             <FormField
               control={form.control}
               name="method"
--- src/features/executions/components/http-request/executor.ts
+++ src/features/executions/components/http-request/executor.ts
@@ -3,6 +3,7 @@ import { NonRetriableError } from "inngest";
 import ky, {type Options as kyOptions } from "ky";
 
 type HttpRequestData = {
+    variableName?:string,
     endpoint?:string,
     method?: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
     body?:string,
@@ -20,6 +21,10 @@ async ({
         // error state for http request
         throw new NonRetriableError("Http request node: no endpoint configured");
     }
+    if(!data.variableName){
+        // error state for http request
+        throw new NonRetriableError("Variable name not configured");
+    }
     const result = await step.run("Http-request",async ()=>{
         const endpoint = data.endpoint!;
         const method = data.method || "GET";
@@ -28,20 +33,25 @@ async ({
 
         if(["POST","PUT","PATCH"].includes(method)){  
                 options.body = data.body;
+                options.headers={
+                    "Content-Type":"application/json"
+                }
         }
 
         const response = await ky(endpoint, options);
-          const contentType = response.headers.get("content-type");
-
+        const contentType = response.headers.get("content-type");
         const responseData = contentType?.includes("application/json")? await response.json():await response.text();
         
-        return {
-            ...context,
+        const responsePayload = {
             httpResponse:{
                 status:response.status,
                 statusText: response.statusText,
                 data: responseData
             }
+        }
+        return {
+            ...context,
+            [data.variableName!]:responsePayload
         }
     });
 
