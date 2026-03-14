baa3dfe16d6d8fc291473764a17fe5750733879d
hemantch01 <hemantchaudhary905@gmail.com>
2026-03-14 06:45:01 +0000

    added request client library && node executed

--- src/features/executions/components/http-request/executor.ts
+++ src/features/executions/components/http-request/executor.ts
@@ -1,15 +1,49 @@
 import type { NodeExecutor } from "@/features/executions/types";
+import { NonRetriableError } from "inngest";
+import ky, {type Options as kyOptions } from "ky";
 
+type HttpRequestData = {
+    endpoint?:string,
+    method?: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
+    body?:string,
+};
 
-type HttpRequestData = Record<string,unknown>;
 export const httpRequestExecutor: NodeExecutor<HttpRequestData>=
 async ({
+    data,
     nodeId,
     context,
     step
 })=>{
     
-    const result = await step.run("http-trigger", async ()=> context);
+    if(!data.endpoint){
+        // error state for http request
+        throw new NonRetriableError("Http request node: no endpoint configured");
+    }
+    const result = await step.run("Http-request",async ()=>{
+        const endpoint = data.endpoint!;
+        const method = data.method || "GET";
+
+        const options: kyOptions = {method};
+
+        if(["POST","PUT","PATCH"].includes(method)){  
+                options.body = data.body;
+        }
+
+        const response = await ky(endpoint, options);
+          const contentType = response.headers.get("content-type");
+
+        const responseData = contentType?.includes("application/json")? await response.json():await response.text();
+        
+        return {
+            ...context,
+            httpResponse:{
+                status:response.status,
+                statusText: response.statusText,
+                data: responseData
+            }
+        }
+    });
 
     return result;
 }
